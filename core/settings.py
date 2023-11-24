12bb3ded9be092bb83dd9c7ae10385e6fc326cbb
Tatiana Sanchez <tatianasanchez03@gmail.com>
2023-11-24 12:15:17 +0100

    Include corsheaders settings

--- core/settings.py
+++ core/settings.py
@@ -43,16 +43,29 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:8000',
+  'http://127.0.0.1:8000',
+  'http://localhost:8080',
+  'http://127.0.0.1:8080',
+)
+
+CORS_ALLOW_HEADERS = list(deafult_headers) + [
+  'contenttype',
 ]
 
 ROOT_URLCONF = 'core.urls'
