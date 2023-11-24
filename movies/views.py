9b170c1b260352bd7f35a5316a96bc639f924d3d
Tatiana Sanchez <tatianasanchez03@gmail.com>
2023-11-24 14:07:00 +0100

    Add view for a list of movies

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+class MoviesListView(APIView):
+  def get(self, request, *args, **kwargs):
+    movies = Movie.objects.all()
+    serializer = Movie(movies, many=True)
+
+    return Response(serializer.data)
+  
