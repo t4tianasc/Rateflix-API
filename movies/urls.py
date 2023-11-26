78943af4d530a21f5670f8ba7a93059f717abfbf
Tatiana Sanchez <tatianasanchez03@gmail.com>
2023-11-26 20:40:59 +0100

    Add new endpoint to get the latest movies released

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import MovieDetailView, MoviesListView
+from .views import MovieDetailView, MoviesListView, LatestMoviesReleasedListView
 
 app_name="movies"
 
 urlpatterns = [
   path('movies/', MoviesListView.as_view()),
-  path('movies/<int:movie_id>/', MovieDetailView.as_view())
+  path('movies/<int:movie_id>/', MovieDetailView.as_view()),
+  path('latest_releases/', LatestMoviesReleasedListView.as_view()),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -18,5 +19,19 @@ class MovieDetailView(APIView):
   def get(self, request, movie_id, *args, **kwargs):
     movie = get_object_or_404(Movie, id=movie_id)
     serializer = MovieSerializer(movie)
+
+    return Response(serializer.data)
+
+
+class LatestMoviesReleasedListView(APIView):
+  def get(self, request, *args, **kwargs):
+    limit_str = request.GET.get('limit', '15')
+    limit = int(limit_str)
+    max_limit = 20
+    if limit > max_limit:
+      return HttpResponse(f"Invalid limit value, max allowed: {max_limit}", status=400)
+
+    movies = Movie.objects.all().order_by('-release_date')[:limit]
+    serializer = MovieSerializer(movies, many=True)
 
     return Response(serializer.data)
